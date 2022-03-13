26c0d84391164b5daf2074dcb557368ec3264be8
Chamith Siriwardena <chamith@gmail.com>
2022-03-13 16:03:25 +0530

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import Button from '@mui/material/Button'
 import logo from './logo.svg';
 import './App.css';
-
+import Dashboard from './components/Dashboard';
+import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Button variant="contained" href="https://reactjs.org">Learn React</Button>
-      </header>
+      </header> */}
+      <Dashboard/>
     </div>
   );
 }
